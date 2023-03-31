243bc1fba9f1118c9808a790075a9d297e5c3ce0
TomaszSokalski <tsokalski88@gmail.com>
2023-03-31 21:19:38 +0200

    add constants

--- src/js/elements.js
+++ src/js/elements.js
@@ -1,10 +1,36 @@
+Math.randomNumberFromRange = function(min, max) {
+    return ~~(Math.random() * (max - min) + min);
+};
+
+Date.formattedDate = function () {
+    const date = new Date();
+    const d = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const y = date.getFullYear()
+
+    return `${y}-${month}-${d}`;
+};
+
 const getElement = (selector) => document.querySelector(selector);
 const getElements = (selector) => document.querySelectorAll(selector);
 
-export const buttons = getElements('button');
-export const tasksTable = getElement('.tasks');
-export const table = getElement('.table');
-export const form = getElement('form');
-export const showButton = getElement('.showDialog');
-export const sortButton = getElement('.sort');
-export const closeButton = getElement('.form-row:last-child > button');
+export const BUTTONS = getElements('button');
+export const TASKS_TABLE = getElement('.tasks');
+export const TABLE = getElement('.table');
+export const FORM = getElement('form');
+export const SHOW_BUTTON = getElement('.showDialog');
+export const SORT_BUTTON = getElement('.sort');
+export const CLOSE_BUTTON = getElement('.form-row:last-child > button');
+export const DIALOG = getElement('dialog');
+
+export const TASKS_STATUS = {
+    nowe: 'nowe',
+    w_trakcie: 'w_trakcie',
+    wykonane: 'wykonane'
+};
+
+export const INIT_PAYLOAD = {
+    id: Math.randomNumberFromRange(1, 1000),
+    date: Date.formattedDate(),
+    status: TASKS_STATUS.nowe,
+};
