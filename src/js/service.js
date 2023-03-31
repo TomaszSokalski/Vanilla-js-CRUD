b1d1de0eda4d569844be71b43275992f6963ef4b
TomaszSokalski <tsokalski88@gmail.com>
2023-03-31 20:45:39 +0200

    add localstorage service

---
+++ src/js/service.js
@@ -0,0 +1,17 @@
+export const localStorageService = {
+    setItem(key, value) {
+        localStorage.setItem(key, JSON.stringify(value));
+    },
+    getItem(key) {
+        return JSON.parse(localStorage.getItem(key));
+    },
+    key(index) {
+        return localStorage.key(index);
+    },
+    removeItem(key) {
+        localStorage.removeItem(key);
+    },
+    clear() {
+        localStorage.clear();
+    },
+};
