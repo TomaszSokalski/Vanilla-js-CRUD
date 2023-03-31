76c2612f5b094fa63fe97cbb7b9a5e87b686737c
TomaszSokalski <tsokalski88@gmail.com>
2023-03-31 16:01:04 +0200

    get html elements

---
+++ src/js/elements.js
@@ -0,0 +1,7 @@
+export const buttons = document.querySelectorAll('button');
+export const tasksTable = document.querySelector('.tasks');
+export const table = document.querySelector('.table');
+export const form = document.querySelector('form');
+export const showButton = document.querySelector('.showDialog');
+export const sortButton = document.querySelector('.sort');
+export const closeButton = document.querySelector('.form-row:last-child > button')
