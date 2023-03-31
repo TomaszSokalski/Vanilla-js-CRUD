27a4cb24d34133e274e758e79feb67ed8a67277b
TomaszSokalski <tsokalski88@gmail.com>
2023-03-31 22:16:48 +0200

    move method to service

--- src/js/service.js
+++ src/js/service.js
@@ -25,3 +25,40 @@ export const getTasks = table => {
     }
     return table;
 };
+
+export const createTable = (table, tasks) => {
+    let tableContent = '';
+
+    for (const el of table) {
+        //language=html
+        tableContent += `
+            <tr>
+               <td class="idValue">${el.id}</td>
+               <td>${el.title}</td>
+               <td>${el.description}</td>
+                <td>
+                    <select name="status" class="status">
+                        <option value="${el.status}">${el.status}</option>
+                        <option value="in_process">in_process</option>
+                        <option value="done">done</option>
+                    </select>
+                </td>
+                <td>${el.date}</td>
+                <td>${el.date_to}</td>
+                <td>
+                    <select name="changeOwner" class="changeOwner">
+                        <option value="">${el.owner}</option>
+                        <option value="user_1">user_1</option>
+                        <option value="user_2">user_2</option>
+                        <option value="user_3">user_3</option>
+                    </select>
+                </td>
+               <td>
+                  <button class="delete-button btn-close">delete</button>
+               </td>
+            </tr>
+         `;
+
+        tasks.innerHTML = tableContent;
+    }
+};
